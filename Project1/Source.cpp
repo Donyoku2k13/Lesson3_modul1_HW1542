3759f908382e5435073d339d989149200340f36f
Александр Горячев <@almaty.shag>
2018-12-05 21:19:53 +0600

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+int main()
+{
+	setlocale(LC_ALL, "");
+	
+	//task10
+	/*int r, d;
+	printf("r");
+	scanf_s("%d", &r);
+	d = 2 * r;
+	printf("result: %d\n", d);*/
+
+	//task9
+	/*int a, p;
+	printf("a");
+	scanf_s("%d", &a);
+	p = 4 * a;
+	printf("result: %d\n", p);*/
+	
+	//task8b
+	/*float  x, a, b;
+	printf("a");
+	scanf_s("%f", &a);
+	printf("b");
+	scanf_s("%f", &b);
+	x = 3.56*a + pow(b, 3) - 5.8*pow(b, 2) + 3.8*a - 1.5;
+	printf("result: %f\n", x);*/
+
+	//task8a
+	/*float  z, x, y;
+	printf("x");
+	scanf_s("%f", &x);
+	printf("y");
+	scanf_s("%f", &y);
+	z = pow(x, 3) - (2.5*x*y) + (1.87*pow(x, 2)) + 1;
+	printf("result: %f\n", z);*/
+	
+	
+	
+	//task7
+	/*float  p, m, v;
+	printf("m");
+	scanf_s("%f", &m);
+	printf("v");
+	scanf_s("%f", &v);
+	p = m/v;
+	printf("result: %f\n", p);
+*/
+
+	//task6
+	/*int x, y, x2, y2, dx,dy;
+	printf("x");
+	scanf_s("%d", &x);
+	printf("y");
+	scanf_s("%d", &y);
+	printf("x2");
+	scanf_s("%d", &x2);
+	printf("y2");
+	scanf_s("%d", &y2);
+	dx = (x2 - x);
+	dy = y2 - y;
+
+	printf("result: %d\n", dx);*/
+
+	//task5
+	/*float  v, a, b, c, s,h;
+	printf("a");
+	scanf_s("%f", &a);
+	printf("b");
+	scanf_s("%f", &b);
+	h = a;
+	c = b;
+	s = a * b;
+	v = s * h;
+	printf("result: %f\n", s);
+	printf("result: %f\n", v);*/
+	
+	
+	
+	//task4
+	/*float v1,v2,s,t;
+	printf("v1");
+	scanf_s("%f", &v1);
+	printf("v2");
+	scanf_s("%f", &v2);
+	printf("s");
+	scanf_s("%f", &s);
+	t = s / (v1 + v2);
+	printf("result: %f\n", t);*/
+	
+	//task3
+	/*float N, nm;
+	printf("N");
+	scanf_s("%f", &N);
+	nm = N / 60;
+	printf("result: %f\n", nm);
+	*/
+	//task2
+	/*float a, x, b, c;
+		printf("a");
+	scanf_s("%f", &a);
+	printf("b");
+	scanf_s("%f", &b);
+	x = -b / a;
+	c = a * x + b;
+	printf("result: %f\n", c);
+	*/
+	
+	//task1
+	/*int x, y, a, z, v;
+	a = 344;
+	printf("x");
+	scanf_s("%d", &x);
+	printf("y");
+	scanf_s("%d", &y);
+	z = x * a;
+	v = y * a;
+	printf("result: %d\n", z);
+	printf("result: %d\n", v);*/
+
+
+	system("pause");
+
+}
